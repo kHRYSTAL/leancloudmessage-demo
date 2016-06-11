302ce28d4469e6431e4131e4b8f25afcdb61f97f
kHRYSTAL <723526676@qq.com>
2016-06-11 23:48:26 +0800

    [20160611] #  add MembersAdapter

---
+++ app/src/main/java/me/khrystal/leancloudmsg/adapter/MembersAdapter.java
@@ -0,0 +1,11 @@
+package me.khrystal.leancloudmsg.adapter;
+
+/**
+ * usage:
+ * author: kHRYSTAL
+ * create time: 16/6/11
+ * update time:
+ * email: 723526676@qq.com
+ */
+public class MembersAdapter {
+}
